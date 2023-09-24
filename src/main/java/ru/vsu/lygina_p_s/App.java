2239c39b3a64990947efc122b29e8f98ac81fe70
Haku <polina231003@mail.ru>
2023-09-24 19:20:58 +0300

    Matrix addition test

--- src/main/java/ru/vsu/lygina_p_s/App.java
+++ src/main/java/ru/vsu/lygina_p_s/App.java
@@ -10,4 +10,17 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+
+    public static int[][] matrixAdd(int[][] m1, int[][] m2) {
+        int[][] res = null;
+        if (m1.length == m2.length && m1[0].length == m2[0].length) {
+            res = new int[m1.length][m1[0].length];
+            for (int i = 0; i < res.length; i++) {
+                for (int j = 0; j < res[0].length; j++) {
+                    res[i][j] = m1[i][j] + m2[i][j];
+                }
+            }
+        }
+        return res;
+    }
 }
--- src/test/java/ru/vsu/lygina_p_s/AppTest.java
+++ src/test/java/ru/vsu/lygina_p_s/AppTest.java
@@ -4,6 +4,8 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import java.util.Arrays;
+
 /**
  * Unit test for simple App.
  */
@@ -38,5 +40,27 @@ public class AppTest
 
     public void test() {
         assertEquals(4, 2 + 2);
+    }
+
+    public void testMatrixAdd() {
+        int[][] m1 = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        int[][] m2 = {
+                {-9, -8, -7},
+                {-6, -5, -4},
+                {-3, -2, -1}
+        };
+        int[][] expected = {
+                {-8, -6, -4},
+                {-2, 0, 2},
+                {4, 6, 8}
+        };
+
+        int[][] actual = App.matrixAdd(m1, m2);
+
+        assertTrue(Arrays.deepEquals(actual, expected));
     }
 }
