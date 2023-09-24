407cccaf58fc0302eb85350e659c99c9afd467dd
Haku <polina231003@mail.ru>
2023-09-24 18:31:08 +0300

    Initial commit

---
+++ src/main/java/ru/vsu/lygina_p_s/App.java
@@ -0,0 +1,13 @@
+package ru.vsu.lygina_p_s;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
